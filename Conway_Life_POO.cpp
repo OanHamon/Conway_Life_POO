991171ac146e62e7e7060b398a5c60ccd7107ea8
BastienLalande <bastien.lalande@viacesi.fr>
2025-11-21 16:27:12 +0100

    using namespace

--- Conway_Life_POO.cpp
+++ Conway_Life_POO.cpp
@@ -3,26 +3,29 @@
 #include <ctime>
 #include <cstdlib>
 
+using namespace std;
+using namespace sf;
+
 const int cellSize = 10;
 const int gridWidth = 80;
 const int gridHeight = 80;
 
-std::vector<std::vector<int>> grid(gridWidth, std::vector<int>(gridHeight));
+vector<vector<int>> grid(gridWidth, vector<int>(gridHeight));
 
 void initializeGrid() {
-    std::srand(std::time(0));
+    srand(time(0));
     for (int x = 0; x < gridWidth; ++x) {
         for (int y = 0; y < gridHeight; ++y) {
-            grid[x][y] = std::rand() % 2;  // Randomly initialize cells as alive or dead
+            grid[x][y] = rand() % 2;  // Randomly initialize cells as alive or dead
         }
     }
 };
 
-void renderGrid(sf::RenderWindow& window) {
+void renderGrid(RenderWindow& window) {
     int x, y;
 
     window.clear();
-    sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
+    RectangleShape cell(Vector2f(cellSize - 1.0f, cellSize - 1.0f));
     for (x = 0; x < gridWidth; ++x) {
         for (y = 0; y < gridHeight; ++y) {
             if (grid[x][y] == 1) {
@@ -35,20 +38,20 @@ void renderGrid(sf::RenderWindow& window) {
 };
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
+    RenderWindow window(VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
 
     initializeGrid();
 
     while (window.isOpen()) {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         renderGrid(window);
 
-        sf::sleep(sf::milliseconds(100));
+        sleep(milliseconds(100));
     }
 
     return 0;
