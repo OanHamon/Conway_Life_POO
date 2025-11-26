11e7b18e2e6b5a07a6f8748f09b31df553bd8f9f
oan.hamon@gmail.com <oan.hamon@gmail.com>
2025-11-26 10:12:45 +0100

    Decomposition de la classe CellState

--- GameData.h
+++ GameData.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstdlib>
 #include <omp.h>
+#include "CellState.h"
 using namespace std;
 
 
@@ -9,41 +10,6 @@ class Cell;
 class Grid;
 class CellState;
 class Rule;
-
-
-class CellState
-{
-public:
-    CellState() : Obstacle(false) {};
-    CellState(bool _ObsState) : Obstacle(_ObsState) {};
-    virtual ~CellState() {}
-    virtual bool isAlive() const = 0;
-    virtual bool isObstacle() const = 0;
-
-protected :
-    bool Obstacle;
-};
-
-class AliveState : public CellState
-{
-public:
-    AliveState() : CellState(false) {}
-    AliveState(bool _ObsState) : CellState(_ObsState) {}
-    bool isAlive() const override { return true; }
-    bool isObstacle() const override { return Obstacle; }
-
-};
-
-class DeadState : public CellState
-{
-public:
-    DeadState() : CellState(false) {}
-    DeadState(bool _ObsState) : CellState(_ObsState) {}
-    bool isAlive() const override { return false; }
-    bool isObstacle() const override { return Obstacle; }
-
-};
-
 
 
 
