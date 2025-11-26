cf045f06c75d9b38a575a5850592bead8532b809
BastienLalande <bastien.lalande@viacesi.fr>
2025-11-26 11:27:07 +0100

    Revert "Merge branch 'Decoomposition' of https://github.com/OanHamon/Conway_Life_POO into Decoomposition"
    This reverts commit 9642cbf581f4cd418f98b981a1aa8525506df988, reversing
    changes made to c8a5979874ba8ff2571cf91f11e57a2d3239d7db.

--- Conway_Life_POO.cpp
+++ Conway_Life_POO.cpp
@@ -3,13 +3,48 @@
 #include <ctime>
 #include <iostream>
 #include <cstdlib>
-#include "Game.h"
+#include "GameLoop.h"
 
 using namespace std;
 using namespace sf;
+
+const int cellSize = 10;
+const int gridWidth = 80;
+const int gridHeight = 80;
+
+Grid* grid;
+
+
+//int main() {
+//    Game game;
+//    game.MainMenu();
+//    srand(time(0));
+//
+//    Rule* conway = new ConwayRule();
+//    Grid* grid = new Grid(gridWidth, gridHeight, conway);
+//
+//    GraphicalDisplay* display = new GraphicalDisplay( gridWidth * cellSize,gridHeight * cellSize,cellSize );
+
+//    while (display->isOpen()) {
+//        display->handleEvents();
+//
+//        display->show(grid);
+//
+//        grid->computeNextGen();
+//
+//        sleep(milliseconds(display->getDelay()));
+//    }
+//
+//    // Nettoyage
+//    delete display;
+//    delete grid;
+//
+//    return 0;
+//}
 
 int main() {
     Game game;
     game.run();
     return 0;
 }
+
---
+++ GameData.h
@@ -0,0 +1,317 @@
+#pragma once
+#include <vector>
+#include <cstdlib>
+#include <omp.h>
+#include "CellState.h"
+using namespace std;
+
+
+class Cell;
+class Grid;
+class CellState;
+class Rule;
+
+
+
+class Cell
+{
+public:
+    Cell(int _row, int _col, CellState* _state)
+        : row(_row), col(_col), currentState(_state), nextState(nullptr) {
+    }
+
+    int getRow() const { return row; }
+    int getCol() const { return col; }
+    CellState* getCurrentState() const { return currentState; }
+    bool isAlive() const { return currentState->isAlive(); }
+
+    void setNextState(CellState* _state)
+    {
+        if (nextState != nullptr) {
+            delete nextState;
+        }
+        nextState = _state;
+    }
+
+    void updateState()
+    {
+        if (nextState != nullptr) {
+            delete currentState;
+            currentState = nextState;
+            nextState = nullptr;
+        }
+    }
+
+private:
+    int row;
+    int col;
+    CellState* currentState;
+    CellState* nextState;
+};
+
+
+class Rule
+{
+public:
+    virtual ~Rule() {}
+    virtual CellState* computeNextState(Cell* cell, Grid* grid) = 0;
+};
+
+class GridData {
+    int rows;
+    int cols;
+    vector<vector<bool>> initialStates;
+};
+
+struct Pattern {
+    string name;
+    vector<pair<int, int>> cells;  // Coordonnées relatives (dx, dy)
+
+    Pattern(const string& n) : name(n) {}
+
+    void addCell(int dx, int dy) {
+        cells.push_back({ dx, dy });
+    }
+};
+
+
+class Grid
+{
+public:
+    Grid(int _rows, int _cols, Rule* _rule)
+        : rows(_rows), cols(_cols), rule(_rule)
+    {
+        cells.resize(rows);
+        for (int i = 0; i < rows; i++) {
+            cells[i].resize(cols);
+            for (int j = 0; j < cols; j++) {
+                int randomValue = rand() % 2;
+                if (randomValue == 1) {
+                    cells[i][j] = new Cell(i, j, new AliveState());
+                }
+                else {
+                    cells[i][j] = new Cell(i, j, new DeadState());
+                }
+            }
+        }
+    }
+    Grid(int _rows, int _cols, Rule* _rule, vector<vector<int>> _data)
+        : rows(_rows), cols(_cols), rule(_rule)
+    {
+        cells.resize(rows);
+        for (int i = 0; i < rows; i++) {
+            cells[i].resize(cols);
+            for (int j = 0; j < cols; j++) {
+                
+                if (_data[i][j] == 1) {
+                    cells[i][j] = new Cell(i, j, new AliveState(false));
+                    
+                }
+                else {
+                    cells[i][j] = new Cell(i, j, new DeadState(false));
+                    
+                }
+            }
+        }
+    }
+
+    ~Grid() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                delete cells[i][j];
+            }
+        }
+        delete rule;
+    }
+
+    int getRows()const { return rows; }
+    int getCols()const { return cols; }
+    Cell* getCell(int _row, int _col)
+    {
+        if (_row < 0 || _row >= rows || _col < 0 || _col >= cols) {
+            return nullptr;
+        }
+        return cells[_row][_col];
+    }
+
+    void setRule(Rule* newRule) {
+        delete rule;
+        rule = newRule;
+    }
+
+    int countAliveNeighbors(Cell* cell)
+    {
+        int x = cell->getRow();
+        int y = cell->getCol();
+        int cmpt = 0;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int n = -1; n <= 1; n++) {
+                if (i == 0 && n == 0) continue;
+
+                int neighborRow = (x + i + rows) % rows;
+                int neighborCol = (y + n + cols) % cols;
+
+               
+               
+               if (cells[neighborRow][neighborCol]->isAlive()) {
+                   cmpt++;
+               }
+                
+            }
+        }
+
+        return cmpt;
+    }
+
+    void computeNextGen()
+    {
+
+        #pragma omp parallel for collapse(2)
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                Cell* cell = cells[i][j];
+                CellState* nxtState = rule->computeNextState(cell, this);
+                if (!cell->getCurrentState()->isObstacle()) { cell->setNextState(nxtState); }
+            }
+        }
+
+
+        UpdateCells();
+    }
+
+    void UpdateCells()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int n = 0; n < cols; n++)
+            {
+                cells[i][n]->updateState();
+            }
+        }
+    }
+
+private:
+    int rows;
+    int cols;
+    vector<vector<Cell*>> cells;
+    Rule* rule;
+};
+
+
+class ConwayRule : public Rule
+{
+public:
+    CellState* computeNextState(Cell* cell, Grid* grid)
+    {
+        bool isCurrentlyAlive = cell->isAlive();
+        int aliveNeighbors = grid->countAliveNeighbors(cell);
+
+        if (isCurrentlyAlive) {
+            // Cellule vivante : survit avec 2 ou 3 voisins
+            if (aliveNeighbors == 2 || aliveNeighbors == 3) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+        else {
+            // Cellule morte : naît avec exactement 3 voisins
+            if (aliveNeighbors == 3) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+    }
+
+};  
+class HardcoreRule : public Rule
+{
+public:
+    CellState* computeNextState(Cell* cell, Grid* grid)
+    {
+        bool isCurrentlyAlive = cell->isAlive();
+        int aliveNeighbors = grid->countAliveNeighbors(cell);
+
+        if (isCurrentlyAlive) {
+            // Cellule vivante : survit avec 2 ou 3 voisins
+            if (aliveNeighbors == 7 || aliveNeighbors == 8) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+        else {
+            // Cellule morte : naît avec exactement 3 voisins
+            if (aliveNeighbors == 8) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+    }
+
+};  
+class LowSocialLifeRule : public Rule
+{
+public:
+    CellState* computeNextState(Cell* cell, Grid* grid)
+    {
+        bool isCurrentlyAlive = cell->isAlive();
+        int aliveNeighbors = grid->countAliveNeighbors(cell);
+
+        if (isCurrentlyAlive) {
+            // Cellule vivante : survit avec 2 ou 3 voisins
+            if (aliveNeighbors ==1 ) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+        else {
+            // Cellule morte : naît avec exactement 3 voisins
+            if (aliveNeighbors == 1) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+    }
+
+};  
+class HighLifeRule : public Rule
+{
+public:
+    CellState* computeNextState(Cell* cell, Grid* grid)
+    {
+        bool isCurrentlyAlive = cell->isAlive();
+        int aliveNeighbors = grid->countAliveNeighbors(cell);
+
+        if (isCurrentlyAlive) {
+            // Cellule vivante : survit avec 2 ou 3 voisins
+            if (aliveNeighbors ==1 || aliveNeighbors == 2 || aliveNeighbors == 3 || aliveNeighbors == 4 || aliveNeighbors == 5 || aliveNeighbors == 8) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+        else {
+            // Cellule morte : naît avec exactement 3 voisins
+            if (aliveNeighbors == 1 || aliveNeighbors == 2 || aliveNeighbors == 3|| aliveNeighbors == 4|| aliveNeighbors == 5|| aliveNeighbors == 6) {
+                return new AliveState();
+            }
+            else {
+                return new DeadState();
+            }
+        }
+    }
+
+};  
