7eed44245e079f38b9aaf8b457c4a6ced7fbdfc1
BastienLalande <bastien.lalande@viacesi.fr>
2025-11-28 14:26:19 +0100

    suppression dossier out de test

--- Conway_Life_POO.test/Conway_Life_POO.test.cpp
+++ Conway_Life_POO.test/Conway_Life_POO.test.cpp
@@ -81,6 +81,40 @@ namespace GridGameCalculationTest
             Assert::IsTrue(outGridInt == attendueGridInt, L"La grille finale ne correspond pas à la grille attendue.");
         }
 
+        TEST_METHOD(TestGrilleCarreObstacle)
+        {
+            // paramètres
+            vector<vector<int>> inGridInt = {
+                {0, 0, 0, 0},
+                {0 ,1, 2, 0},
+                {0 ,2, 1, 0},
+                {0, 0, 0, 0}
+            };
+
+            vector<vector<int>> attendueGridInt = {
+                {0, 0, 0, 0},
+                {0 ,1, 2, 0},
+                {0 ,2, 1, 0},
+                {0, 0, 0, 0}
+            };
+
+            int nIter = 5;
+            ConwayRule rule;
+
+
+
+            // test
+            Grid grid(inGridInt.size(), inGridInt[0].size(), &rule, inGridInt);
+
+            for (int i = 0; i < nIter; i++)
+            {
+                grid.computeNextGen();
+            }
+
+            vector<vector<int>> outGridInt = grid.getGridInt();
+            Assert::IsTrue(outGridInt == attendueGridInt, L"La grille finale ne correspond pas à la grille attendue.");
+        }
+
         TEST_METHOD(TestGrilleBlinker)
         {
             // paramètres
